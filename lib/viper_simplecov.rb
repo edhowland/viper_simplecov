0b0703681ef7091baa124a261419eea4dccdc6aa
edhowland <ed.howland@gmail.com>
2016-03-01 01:35:52 +0000

    \'made spec/load_path.rb depend on viper --lib-path\'

--- lib/viper_simplecov.rb
+++ lib/viper_simplecov.rb
@@ -1,5 +1,9 @@
 # viper_simplecov.rb - Initialization for viper_simplecov Viper package
 
+require_relative 'tools/load_cov'
+require_relative 'tools/cov'
+require_relative 'tools/cov_report'
+
 # Install the load_cov, cov and cov_report commands:
 Viper::Session[:commands][:load_cov] = ->(_b, *args) { load_cov args[0]; say "Coverage repor #{args[0]} loaded" } 
 Viper::Session[:commands][:cov] = ->(b, *_args) { sc = ScratchBuffer.new; sc.name = "Coverage report for #{b.name}"; cov(sc, b.name); $buffer_ring.unshift sc; sc.beg; say sc.name; say sc.line }
---
+++ spec/load_path.rb
@@ -0,0 +1,12 @@
+# load_path.rb - extends $LOAD_PATH with Viper paths
+
+# path to viper executable
+viper_path = '/home/vagrant/src/viper/bin/viper'
+if system("#{viper_path} --ok")
+  viper_lib = %x{#{viper_path} --lib-path}.chomp
+else
+  raise RuntimeError.new('Could not find viper executable')
+end
+
+$LOAD_PATH.unshift viper_lib
+require 'viper'
