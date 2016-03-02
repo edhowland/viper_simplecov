e8c9283658a8a9edd75774985d0591149d9b3d11
edhowland <ed.howland@gmail.com>
2016-03-02 01:33:17 +0000

    \'added Viper::Packages::ViperSimplecov::VERSION\'

--- lib/viper_simplecov.rb
+++ lib/viper_simplecov.rb
@@ -1,5 +1,7 @@
 # viper_simplecov.rb - Initialization for viper_simplecov Viper package
 
+require_relative 'viper_simplecov/version'
+
 require_relative 'tools/load_cov'
 require_relative 'tools/cov'
 require_relative 'tools/cov_report'
---
+++ lib/viper_simplecov/version.rb
@@ -0,0 +1,13 @@
+# version.rb - Version for Viper::Package::ViperSimplecov
+
+# TODO module documentation
+module Viper
+  # TODO module documentation
+  module Packages
+    # TODO module documentation
+    module ViperSimplecov
+      VERSION = '0.1.0'.freeze
+    end
+  end
+end
+
