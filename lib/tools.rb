209fb3ba9f6c236fa277a5f28d534eb1d033ac91
edhowland <ed.howland@gmail.com>
2016-03-03 17:09:58 +0000

    \'modified cov_report to use improved path_shortener: version used relative_to_here. now pathnames are relative to current dir of Viper editor process\'

--- lib/tools.rb
+++ lib/tools.rb
@@ -1,6 +1,7 @@
 # tools.rb -  requires for tools/
 
 require_relative 'tools/path_subtract'
+require_relative 'tools/path_shortener'
 
 require_relative 'tools/load_cov'
 require_relative 'tools/cov'
