89c51c617529838c9053aea2917d7b59e9b12ac8
edhowland <ed.howland@gmail.com>
2016-03-03 18:53:18 +0000

    \'separated perform! in cov_file to first open file and then run cov on file\'

--- lib/viper_simplecov.rb
+++ lib/viper_simplecov.rb
@@ -7,4 +7,4 @@ require_relative 'tools'
 Viper::Session[:commands][:load_cov] = ->(_b, *args) { load_cov args[0]; say "Coverage repor #{args[0]} loaded" } 
 Viper::Session[:commands][:cov] = ->(b, *_args) { sc = ScratchBuffer.new; sc.name = "Coverage report for #{b.name}"; cov(sc, b.name); $buffer_ring.unshift sc; sc.beg; say sc.name; say sc.line }
 Viper::Session[:commands][:cov_report] = ->(_b, *_args) { cov_report; say $buffer_ring[0].name.to_s }
-
+Viper::Session[:commands][:cov_file] = ->(b, *args) { cov_file b }
